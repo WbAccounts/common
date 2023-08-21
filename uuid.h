9737e6e2e4930a7e028bcc76c0eb1280a4f66873
WbAccounts <WbAccounts@163.com>
2023-08-21 10:16:33 +0800

    :sparkles: feat(1.0.0.1000): 添加uuid common 文件
    Signed-off-by: WbAccounts <WbAccounts@163.com>

---
+++ uuid.h
@@ -0,0 +1,23 @@
+#ifndef UUID_H_
+#define UUID_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
+
+#define UUID_LEN 37
+
+namespace uuid {
+
+enum UUID_STATUS{
+    UUID_ESUCCESS =  0,
+    UUID_EFAILURE = -1
+};
+
+UUID_STATUS uuid4_generate(char *dst);
+UUID_STATUS uuid4_generate(std::string &dst);
+std::string uuid4_generate();
+} // namespace
+
+#endif /* UUID_H_*/ 
+ 
